a3b2310473efa5cd36d483b40ededb482a67a0c0
koc9n <kos.mironchik@gmail.com>
2024-12-20 06:04:08 +0700

    added logging for strategy choice

--- src/main/kotlin/org/test/anymindtask/strategy/PricingStrategyFactory.kt
+++ src/main/kotlin/org/test/anymindtask/strategy/PricingStrategyFactory.kt
@@ -1,15 +1,20 @@
 package org.test.anymindtask.strategy
 
+import org.slf4j.LoggerFactory
 import org.test.anymindtask.model.enum.PaymentMethod
 
 object PricingStrategyFactory {
+    private val logger = LoggerFactory.getLogger(PricingStrategyFactory::class.java)
+
     fun getStrategy(paymentMethod: PaymentMethod): PricingStrategy {
-        return when (paymentMethod) {
+        val strategy = when (paymentMethod) {
             PaymentMethod.CASH, PaymentMethod.CASH_ON_DELIVERY -> CashStrategy()
             PaymentMethod.VISA, PaymentMethod.MASTERCARD, PaymentMethod.JCB -> CardStrategy()
             PaymentMethod.AMEX -> AmexStrategy()
             PaymentMethod.LINE_PAY, PaymentMethod.PAYPAY, PaymentMethod.GRAB_PAY -> LinePayStrategy()
             PaymentMethod.POINTS, PaymentMethod.BANK_TRANSFER, PaymentMethod.CHEQUE -> NoPointsStrategy()
         }
+        logger.debug("Chosen strategy for payment method {}: {}", paymentMethod, strategy::class.simpleName)
+        return strategy
     }
 }
